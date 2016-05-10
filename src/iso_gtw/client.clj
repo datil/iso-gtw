d9137485900aff6efdba3bef5755c80164298468
Eduardo Raad <eduardo.raad@datilmedia.com>
2016-05-10 14:39:11 -0400

    Adds sample charge transaction.

--- src/iso_gtw/client.clj
+++ src/iso_gtw/client.clj
@@ -23,12 +23,21 @@
                   true)
                 (onMessage [this ctx iso-msg]
                   (println "server response code: "
-                           (.getField iso-msg 39))
+                           (.debugString iso-msg))
                   true)))
 
-(.addMessageListener client mti-0810)
+(def mti-0210 (reify IsoMessageListener
+                (applies [this iso-msg]
+                  (println (.debugString iso-msg))
+                  true)
+                (onMessage [this ctx iso-msg]
+                  (println "server response"
+                           (.debugString iso-msg)))))
 
-(defn mti-800-msg
+(.addMessageListener client mti-0810)
+(.addMessageListener client mti-0210)
+
+(defn mti-0800-msg
   []
   (let [iso-msg (.newMessage msg-factory 0x800)]
     (doto iso-msg
@@ -37,6 +46,34 @@
       (.setField 11 (.value IsoType/NUMERIC 123456 6))
       (.setField 32 (.value IsoType/NUMERIC 921802 6))
       (.setField 70 (.value IsoType/NUMERIC 000 3)))))
+
+(defn mti-0200-msg
+  []
+  (let [iso-msg (.newMessage msg-factory 0x020)]
+    (doto iso-msg
+      (.setField 2 (.value IsoType/ALPHA "165555555555554444" 18))
+      (.setField 3 (.value IsoType/ALPHA "003000" 6))
+      (.setField 4 (.value IsoType/ALPHA "000000001000" 12))
+      (.setField 7 (.value IsoType/DATE10 "0509095721"))
+      (.setField 11 (.value IsoType/ALPHA "000001" 6))
+      (.setField 12 (.value IsoType/ALPHA "095721" 6))
+      (.setField 13 (.value IsoType/ALPHA "0509" 4))
+      (.setField 14 (.value IsoType/ALPHA "1804" 4))
+      (.setField 15 (.value IsoType/ALPHA "0509" 4))
+      (.setField 18 (.value IsoType/ALPHA "7832" 4))
+      (.setField 22 (.value IsoType/ALPHA "012" 3))
+      (.setField 32 (.value IsoType/ALPHA "06921802" 8))
+      (.setField 33 (.value IsoType/ALPHA "06921802" 8))
+      (.setField 37 (.value IsoType/ALPHA "621010000999" 12))
+      (.setField 41 (.value IsoType/ALPHA "APP66699" 8))
+      (.setField 42 (.value IsoType/ALPHA "1000000209     " 15))
+      (.setField 43 (.value IsoType/ALPHA "DATILMEDIA PACIFICARD   Gy           ECU" 40))
+      (.setField 48 (.value IsoType/ALPHA "0089203683" 10))
+      (.setField 49 (.value IsoType/ALPHA "840" 3))
+      (.setField 54 (.value IsoType/ALPHA "0161112000000000000" 19))
+      (.setField 57 (.value IsoType/ALPHA "00800003000" 11))
+      (.setField 58 (.value IsoType/ALPHA "024220100000000000000000000" 27))
+      (.setField 61 (.value IsoType/ALPHA "02610250000006002180000000000" 29)))))
 
 (defn connect
   [client host port]
