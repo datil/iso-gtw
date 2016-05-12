5d4790b255cdeb96b0fdfc22090188b58c4f3125
Eduardo Raad <eduardo.raad@datilmedia.com>
2016-05-12 14:58:58 -0400

    Defines charge message as specified in BP's example.

--- src/iso_gtw/client.clj
+++ src/iso_gtw/client.clj
@@ -1,4 +1,5 @@
 (ns iso-gtw.client
+  (:require [byte-streams :as b])
   (:import [org.jreactive.iso8583.client Iso8583Client]
            [org.jreactive.iso8583 IsoMessageListener]
            [com.solab.iso8583 IsoMessage]
@@ -12,7 +13,7 @@
 
 (.setCharacterEncoding msg-factory (.name StandardCharsets/US_ASCII))
 (.setUseBinaryMessages msg-factory true)
-(.setUseBinaryBitmap msg-factory true)
+;(.setUseBinaryBitmap msg-factory true)
 
 (def client (new Iso8583Client msg-factory))
 
@@ -50,31 +51,31 @@
 
 (defn mti-0200-msg
   []
-  (let [iso-msg (.newMessage msg-factory 0x020)]
+  (let [iso-msg (.newMessage msg-factory 0x200)]
     (doto iso-msg
-      (.setField 2 (.value IsoType/LLVAR "5555555555554444"))
-      (.setField 3 (.value IsoType/ALPHA "003000" 6))
-      (.setField 4 (.value IsoType/ALPHA "000000001000" 12))
-      (.setField 7 (.value IsoType/DATE10 "0509095721"))
-      (.setField 11 (.value IsoType/ALPHA "000001" 6))
-      (.setField 12 (.value IsoType/ALPHA "095721" 6))
-      (.setField 13 (.value IsoType/ALPHA "0509" 4))
-      (.setField 14 (.value IsoType/ALPHA "1804" 4))
-      (.setField 15 (.value IsoType/ALPHA "0509" 4))
-      (.setField 18 (.value IsoType/ALPHA "7832" 4))
-      (.setField 22 (.value IsoType/ALPHA "012" 3))
-      (.setField 32 (.value IsoType/LLVAR "921802"))
-      (.setField 33 (.value IsoType/LLVAR "921802"))
-      (.setField 37 (.value IsoType/ALPHA "621010000999" 12))
-      (.setField 41 (.value IsoType/ALPHA "APP66699" 8))
-      (.setField 42 (.value IsoType/ALPHA "1000000209     " 15))
-      (.setField 43 (.value IsoType/ALPHA "DATILMEDIA PACIFICARD   Gy           ECU" 40))
-      (.setField 48 (.value IsoType/LLLVAR "9203683"))
-      (.setField 49 (.value IsoType/ALPHA "840" 3))
-      (.setField 54 (.value IsoType/LLLVAR "1112000000000000"))
-      (.setField 57 (.value IsoType/LLLVAR "00003000"))
-      (.setField 58 (.value IsoType/LLLVAR "220100000000000000000000"))
-      (.setField 61 (.value IsoType/LLLVAR "10250000006002180000000000")))))
+      (.setField 2 (.value IsoType/LLBIN (b/to-byte-array "5555555555554444")))
+      (.setField 3 (.value IsoType/BINARY (b/to-byte-array "003000") 6))
+      (.setField 4 (.value IsoType/BINARY (b/to-byte-array "000000001000") 12))
+      (.setField 7 (.value IsoType/BINARY (b/to-byte-array "0509095721") 10))
+      (.setField 11 (.value IsoType/BINARY (b/to-byte-array "000001") 6))
+      (.setField 12 (.value IsoType/BINARY (b/to-byte-array "095721") 6))
+      (.setField 13 (.value IsoType/BINARY (b/to-byte-array "0509") 4))
+      (.setField 14 (.value IsoType/BINARY (b/to-byte-array "1804") 4))
+      (.setField 15 (.value IsoType/BINARY (b/to-byte-array "0509") 4))
+      (.setField 18 (.value IsoType/BINARY (b/to-byte-array "7832") 4))
+      (.setField 22 (.value IsoType/BINARY (b/to-byte-array "012") 3))
+      (.setField 32 (.value IsoType/LLBIN (b/to-byte-array "921802")))
+      (.setField 33 (.value IsoType/LLBIN (b/to-byte-array "921802")))
+      (.setField 37 (.value IsoType/BINARY (b/to-byte-array "621010000999") 12))
+      (.setField 41 (.value IsoType/BINARY (b/to-byte-array "APP66699") 8))
+      (.setField 42 (.value IsoType/BINARY (b/to-byte-array "1000000209     ") 15))
+      (.setField 43 (.value IsoType/BINARY (b/to-byte-array "DATILMEDIA PACIFICARD   Gy           ECU") 40))
+      (.setField 48 (.value IsoType/LLLBIN (b/to-byte-array "9203683")))
+      (.setField 49 (.value IsoType/BINARY (b/to-byte-array "840") 3))
+      (.setField 54 (.value IsoType/LLLBIN (b/to-byte-array "1112000000000000")))
+      (.setField 57 (.value IsoType/LLLBIN (b/to-byte-array "00003000")))
+      (.setField 58 (.value IsoType/LLLBIN (b/to-byte-array "220100000000000000000000")))
+      (.setField 61 (.value IsoType/LLLBIN (b/to-byte-array "10250000006002180000000000"))))))
 
 (defn connect
   [client host port]
